aaa33be234a83b0dec37c2c26d309ef1713041e4
AlejandroLeyvaa <64874435+AlejandroLeyvaa@users.noreply.github.com>
2020-11-03 12:38:07 -0700

    Correcciones en el esquema de los contenidos

--- api/utils/schemas/content.js
+++ api/utils/schemas/content.js
@@ -2,12 +2,14 @@ const joi = require('joi');
 
 const postIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/);
 const postTitleSchema = joi.string().max(80);
+const postSlugSchema = joi.string().max(16);
 const postContent = joi.string().max(6000);
 const postImage = joi.string().uri();
 const postTag = joi.string().min(1).max(64);
 const postAuthor = joi.string().min(5).max(64);
 
 const createPostSchema = {
+  slug: postSlugSchema.required(),
   title: postTitleSchema.required(),
   content: postContent.required(),
   author: postAuthor.required(),
